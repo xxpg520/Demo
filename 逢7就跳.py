b96a8c6839fb1d8352a3356b34ff900c1abfd97d
xxpg520 <xxpg520@gmail.com>
2021-09-25 23:48:59 +0800

    Create 逢7就跳.py

---
+++ 逢7就跳.py
@@ -0,0 +1,14 @@
+for a in range(1, 101):
+    if a % 7 == 0:
+        continue
+    else:
+        print(a)
+# a = 0
+
+# while a <= 99:
+#     a += 1
+#     if a % 7 == 0:
+#         continue
+#     else:
+#         print(a)
+#
